b018f96375f48fb97a6af3b883f32ad4d056304c
Serhat Yurdakul <serhatyurdakulsl@gmail.com>
2023-06-27 18:41:09 +0300

    Initially used useEffect to restore data
    Using useEffect, the data in the json server is fetched when the page is refreshed. And added features to fetch data using useEffect and delete and update with axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
+import axios from "axios";
 import "./App.css";
 import CreateTask from "./components/CreateTask";
 import ListTasks from "./components/ListTasks";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
+  let usedIds = [];
+  const createTask = async (title, taskDescription) => {
+    const response = await axios.post("http://localhost:3000/tasks", {
+      title,
+      taskDescription,
+    });
+
+    let id;
+    do {
+      id = Math.round(Math.random() * 99999);
+    } while (usedIds.includes(id));
+
+    const createdTask = [...tasks, response.data];
+    // usedIds.push(id);
+    usedIds = [...usedIds, id];
+
+    /* push ile spread farkı:push her defasında yeni bir referans oluşturmaz. spread is her defasında yeni bir kopya ve referans oluşturur. spread kulllanıldığında orjinal veri etkilenmez iken push kullandığımızda orjinal veri etkilenir. Ayrıca spread operatörü kullanıldığında her defasında yeniden render edilmesi sağlanıyor. push metodu kullanmış olsaydık eski arrayin üzerine eklendiği için diziye yeni eleman eklendiğinde render edilmez. Eğer biz bir diziye her yeni eleman eklediğimizde yeniden render edilmesini istiyorsak push yerine spread operatörünü kullanmamız gerekiyor. */
+
+    setTasks(createdTask);
+  };
+
+  const fetchTasks = async (id) => {
+    const response = await axios.get("http://localhost:3000/tasks");
+    setTasks(response.data);
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []); /* Sadece başlangıçta çalışmasını için sonuna array eklendi */
+
   const [tasks, setTasks] = useState([]);
-  const editTaskById = (id, updatedTitle, updatedTaskDesc) => {
+  const editTaskById = async (id, updatedTitle, updatedTaskDesc) => {
+    await axios.put(`http://localhost:3000/tasks/${id}`, {
+      title: updatedTitle,
+      taskDescription: updatedTaskDesc,
+    });
     const updatedTasks = tasks.map((task) => {
       if (task.id === id) {
         return { id, title: updatedTitle, taskDescription: updatedTaskDesc };
@@ -15,39 +50,17 @@ function App() {
     });
     setTasks(updatedTasks);
   };
-  const deleteTaskById = (id) => {
+  const deleteTaskById = async (id) => {
+    await axios.delete(`http://localhost:3000/tasks/${id}`);
     const afterDeletingTasks = tasks.filter((task) => {
       return task.id !== id;
     });
     setTasks(afterDeletingTasks);
   };
 
-  let usedIds = [];
-  const addTask = (title, taskDescription) => {
-    let id;
-    do {
-      id = Math.round(Math.random() * 99999);
-    } while (usedIds.includes(id));
-
-    const addedTasks = [
-      ...tasks,
-      {
-        id,
-        title,
-        taskDescription,
-      },
-    ];
-    // usedIds.push(id);
-    usedIds = [...usedIds, id];
-
-    /* push ile spread farkı:push her defasında yeni bir referans oluşturmaz. spread is her defasında yeni bir kopya ve referans oluşturur. spread kulllanıldığında orjinal veri etkilenmez iken push kullandığımızda orjinal veri etkilenir. Ayrıca spread operatörü kullanıldığında her defasında yeniden render edilmesi sağlanıyor. push metodu kullanmış olsaydık eski arrayin üzerine eklendiği için diziye yeni eleman eklendiğinde render edilmez. Eğer biz bir diziye her yeni eleman eklediğimizde yeniden render edilmesini istiyorsak push yerine spread operatörünü kullanmamız gerekiyor. */
-
-    setTasks(addedTasks);
-  };
-
   return (
     <div className="App">
-      <CreateTask onCreate={addTask} />
+      <CreateTask onCreate={createTask} />
       <h1>Tasks</h1>
       <ListTasks
         tasks={tasks}
